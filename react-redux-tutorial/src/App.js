341d13667461aaa2cba36e7653d311690c011555
manikarthi25 <manikarthi25@gmail.com>
2022-07-17 10:41:16 +0530

    redux code

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import { increment, decrement, isLogged }from './actions';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+const counter = useSelector((state) => state.counter);
+const logged = useSelector((state) => state.login);
+
+const dispatch = useDispatch();
+
+let sign_in_status = '';
+if(logged === false) {
+  sign_in_status = "SIGN_IN";
+} else {
+  sign_in_status = "SIGN_OUT";
+}
+
   return (
     <div className="App">
-      <header className="App-header">
         <h1>React Redux Tutorial</h1>
-      </header>
+        <p>Counter : {counter}</p>
+
+        <button onClick={() => dispatch(increment())}>INCREMEENT</button>
+        <button onClick={() => dispatch(decrement())}>DECREMEENT</button>
+        <button onClick={() => dispatch(isLogged())}>{sign_in_status}</button>
+
+        {logged ? <p> Login</p> : <p>Logout</p>}
     </div>
   );
 }
